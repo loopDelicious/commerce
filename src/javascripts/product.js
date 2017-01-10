42c7e727f3eca56d59384ff7473823fa898ae5f9
Joyce Lin <joycelin79@gmail.com>
2017-01-10 14:49:17 -0800

    display product image, name and price

---
+++ src/javascripts/product.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Product extends Component {
+
+    state = {
+        products: []
+    };
+
+    componentDidMount = () => {
+        $.ajax({
+            url: 'https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js',
+            type: 'get',
+            contentType: 'application/json',
+            success: (response) => {
+
+                var resObj = JSON.parse(response);
+
+                this.setState({
+                    products: resObj.products
+                });
+            }
+        })
+    };
+
+    render() {
+
+        var productRows = this.state.products.map( (product, i) => {
+            return (
+
+                <tr key={i}>
+                    <td><img src={product.mainImage.ref} alt={product.name} /></td>
+                    <td>{product.name}</td>
+                    <td>{product.minPrice}</td>
+                </tr>
+            )
+        });
+
+        return (
+            <div>
+
+                { this.state.products.length > 0 ?
+
+                    <div id="partDetails">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td><h4>Image</h4></td>
+                                    <td><h4>Item</h4></td>
+                                    <td><h4>Price</h4></td>
+                                </tr>
+                            </thead>
+                            <tbody>{productRows}</tbody>
+                        </table>
+                    </div>
+
+                : null }
+
+            </div>
+        )
+    }
+}
+
+export default Product;
